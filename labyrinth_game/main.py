a37b28c389dc43e04bc75b651572be3c6ff68843
Ekrusakov <Ekrusakov04@gmail.com>
2025-11-01 21:36:48 +0300

    2.1 Understanding data structure

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+
+from .constants import ROOMS
+
 def main():
     print("Первая попытка запустить проект!")
 
 if __name__ == "__main__":
     main()
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
+  
